fdb86979fddf7989c258cbb61609f4101334282b
josh <josh.ribakoff@gmail.com>
2013-10-13 06:28:09 -0400

    added ability to specify number of sample products to create

--- create.php
+++ create.php
@@ -1,12 +1,26 @@
 <?php
 require_once 'vendor/autoload.php';
+
+use GetOptionKit\GetOptionKit;
+
+$getopt = new GetOptionKit;
+$getopt->add( 'n|number:=i' , 'option requires a integer value' );
+
+try {
+    $result = $getopt->parse( $argv );
+    $number = $result->number ? $result->number:10;
+} catch(Exception $e) {
+    echo 'Try: create.php --number=10';
+    exit;
+}
+
 $seedFile = 'seed.csv';
 $reader = new Csv_Reader($seedFile, new Csv_Dialect());
 $headerRow = $reader->getAssociativeRow();
 $seedRow = $reader->getAssociativeRow();
 $writer = new Csv_Writer(STDOUT);
 $writer->writeRow($headerRow);
-for($i=1; $i<=10; $i++) {
+for($i=1; $i<=$number; $i++) {
     $productRow = array_merge($seedRow, array(
         'sku' => 'sku-'.$i,
         'name' => 'product '.$i,
